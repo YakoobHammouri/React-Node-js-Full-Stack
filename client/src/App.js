86ec83bbb0df3ceff148c6da2cf952115285bdf2
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-04-20 00:17:38 +0300

    add Drawer
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,24 @@ import User from './components/user';
 import MaterialUI from './components/Material-UI';
 import TextBox from './components/TextBox';
 import Tabs from './components/Tabs';
+import MiniDrawer from './components/Drawers';
 import Typography from '@material-ui/core/Typography';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>React & Node js Full Stack</p>
-      </header>
-      <Typography variant="body1" align="center" gutterBottom>
-        User Date From Node js Server run at port 3001
-      </Typography>
-      <User />
-      <h1>Material UI Components</h1>
-      <MaterialUI />
-      {/* <TextBox /> */}
-      <Tabs />
+      <MiniDrawer>
+        <header className="App-header">
+          <p>React & Node js Full Stack</p>
+        </header>
+        <Typography variant="body1" align="center" gutterBottom>
+          User Date From Node js Server run at port 3001
+        </Typography>
+        <User />
+        <h1>Material UI Components</h1>
+        <MaterialUI />
+        {/* <TextBox /> */}
+        <Tabs />
+      </MiniDrawer>
     </div>
   );
 }
